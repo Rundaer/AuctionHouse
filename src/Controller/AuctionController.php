1bbf20d41c47e75128bc6dbd17dbcc02995a053a
rundaer <rundaer@gmail.com>
2020-04-12 13:54:49 +0200

    auction entity / list auction from data base / add auction form

--- src/Controller/AuctionController.php
+++ src/Controller/AuctionController.php
@@ -2,9 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Auction;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class AuctionController extends AbstractController
 {
@@ -15,16 +22,8 @@ class AuctionController extends AbstractController
      */
     public function indexAction()
     {
-        $auctions = [];
-
-        for ($i = 1; $i < 5; $i++){
-            array_push($auctions, [
-                'id'            => $i,
-                'title'         => 'Aukcja '.$i,
-                'description'   => 'Opis aukcji '.$i,
-                'price'         => 5 * $i,
-            ]);
-        }
+        $entityManager = $this->getDoctrine()->getManager();
+        $auctions = $entityManager->getRepository(Auction::class)->findAll();
 
         return $this->render('auction/index.html.twig', ['auctions' => $auctions]);
     }
@@ -32,12 +31,44 @@ class AuctionController extends AbstractController
     /**
      * Details Action, shows details of certain auction
      * 
-     * @Route("/{id}", name="auction_details")
+     * @Route("/auction/{id}", name="auction_details")
      * 
      * @param $id
      */
     public function detailsAction($id)
     {
         return $this->render('auction/details.html.twig');
+    }
+
+    /**
+     * Form to add auction
+     * 
+     * @Route("/add", name="auction_add")
+     * 
+     * @return Response
+     */
+    public function addAction(Request $request)
+    {
+        $auction = new Auction();
+
+        $form = $this->createFormBuilder($auction)
+            ->add('title',          TextType::class)
+            ->add('description',    TextareaType::class)
+            ->add('price',          NumberType::class)
+            ->add('submit',         SubmitType::class)
+            ->getForm();
+
+        if ($request->isMethod('post')){
+            $form->handleRequest($request);
+
+            $auction = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($auction);
+            $entityManager->flush();
+
+            return $this->redirectToRoute("auction_index");
+        }
+        return $this->render('auction/add.html.twig', ["form" => $form->createView()]);
     }
 }
