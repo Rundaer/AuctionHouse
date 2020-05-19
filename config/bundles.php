c07b7e4ac428b7b648410dc5a47c5746a900d5bd
rundaer <rundaer@gmail.com>
2020-05-19 22:22:19 +0200

    packages update / FOSuser-bundle ...

--- config/bundles.php
+++ config/bundles.php
@@ -13,5 +13,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    FOS\UserBundle\FOSUserBundle::class => ['all' => true],
 ];
